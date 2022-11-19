e105892d2917085537672473c4d64ef24b9318cf
Natalia <nata22808@mail.ru>
2022-11-20 00:17:51 +0300

    set up logic

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    override func viewWillLayoutSubviews() {
+        redLightView.layer.cornerRadius = redLightView.bounds.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.bounds.width / 2
+        greenLightView.layer.cornerRadius = greenLightView.bounds.width / 2
+        startButton.layer.cornerRadius = 10
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    
+    @IBAction func startButtonDidTapped() {
+        redLightView.alpha = 1
+        
+    }
+    
 }
 
